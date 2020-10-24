5ecfcb5579b0673736938ed911086597724307f7
Jeremy Rajan <jeremyrajan@gmail.com>
2020-10-24 20:48:59 +0800

    more changes

--- src/templates/work.js
+++ src/templates/work.js
@@ -8,25 +8,25 @@ import Layout from "../components/layout"
 export default ({ data }) => (
   <Layout>
     <article className="sheet">
-      <HelmetDatoCms seo={data.datoCmsWork.seoMetaTags} />
+      <HelmetDatoCms seo={data.datoCmsPost.seoMetaTags} />
       <div className="sheet__inner">
-        <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
-        <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
+        <h1 className="sheet__title">{data.datoCmsPost.title}</h1>
+        <p className="sheet__lead">{data.datoCmsPost.excerpt}</p>
         <div className="sheet__slider">
           <Slider infinite={true} slidesToShow={2} arrows>
-            {data.datoCmsWork.gallery.map(({ fluid }) => (
-              <img alt={data.datoCmsWork.title} key={fluid.src} src={fluid.src} />
+            {data.datoCmsPost.gallery.map(({ fluid }) => (
+              <img alt={data.datoCmsPost.title} key={fluid.src} src={fluid.src} />
             ))}
           </Slider>
         </div>
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
-            __html: data.datoCmsWork.descriptionNode.childMarkdownRemark.html,
+            __html: data.datoCmsPost.descriptionNode.childMarkdownRemark.html,
           }}
         />
         <div className="sheet__gallery">
-          <Img fluid={data.datoCmsWork.coverImage.fluid} />
+          <Img fluid={data.datoCmsPost.coverImage.fluid} />
         </div>
       </div>
     </article>
@@ -35,7 +35,7 @@ export default ({ data }) => (
 
 export const query = graphql`
   query WorkQuery($slug: String!) {
-    datoCmsWork(slug: { eq: $slug }) {
+    datoCmsPost(slug: { eq: $slug }) {
       seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
       }
